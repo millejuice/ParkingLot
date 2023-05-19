fd5ab87437f0cbcfd950ae7b3397271765445c1b
incheolWee <dnldlscjf@gmail.com>
2023-05-19 14:16:57 +0900

    printmenu(); 함수를 삭제

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include "menu.h"
 typedef struct
 {
     // 사용해야하는 변수
@@ -25,7 +25,6 @@ typedef struct
 
 } parking;
 
-int printMenu();                                // 메뉴를 출력하는 함수
 int addData(parking *s, parking *a, int count); // 데이터를 출력하는 함수
 int fixData(parking *s, parking *a, int count); // 데이터를 수정하는 함수
 void deleteData(parking* p,int count);
@@ -177,28 +176,6 @@ void deleteData(parking* p, int count)
                 break;
             }
         }
-}
-
-int printMenu()
-{
-    int result;
-    printf("--------------주차 관리 시스템---------------\n");
-    printf("|                                           |\n");
-    printf("--------------메뉴를 선택하세요--------------\n");
-    printf("|   1. 데이터 추가                          |\n");
-    printf("|   2. 데이터 변경                          |\n");
-    printf("|   3. 데이터 출력                          |\n"); // list형식
-    printf("|   4. 데이터 삭제                          |\n");
-    printf("|   5. 요금 정산하기                        |\n");
-    printf("|   6. 남은 자리 확인하기                   |\n");
-    printf("|   7. 내 자동차 찾기                       |\n");
-    printf("|   8. 지불할 금액 & 거스름돈               |\n");
-    printf("|   0. 종료                                 |\n");
-    printf("---------------------------------------------\n");
-
-    scanf("%d", &result);
-
-    return result;
 }
 
 int printCharge(parking *p)
